166dc0fbd47a8513b81ea6737b9c0afd6709792a
victor-romero-martinez <victordav147@gmail.com>
2026-05-16 17:24:40 -0300

    feat: add animation page elements and test case for slide message functionality

---
+++ tests/test_slide_msessage.py
@@ -0,0 +1,16 @@
+from core.actions import *
+from core.assertions import *
+from core.waits import wait_for_animation_end, wait_invisible
+from pages.animation_page import *
+
+
+def test_animation_slide():
+    assert_visible(COMPANY_NAME_LABEL)
+    tap_on(ANIMATION_DEMO_BUTTON)
+    tap_on(SLIDE_MESSAGE_BUTTON)
+
+    assert_visible(SLIDE_MESSAGE)
+    wait_invisible(SLIDE_MESSAGE)
+
+    back()
+    assert_visible(COMPANY_NAME_LABEL)
