324ebbf2bccebde987047e8dde04deaa7970331e
victor-romero-martinez <victordav147@gmail.com>
2026-05-16 17:24:40 -0300

    feat: add animation page elements and test case for slide message functionality

---
+++ pages/animation_page.py
@@ -0,0 +1,6 @@
+from appium.webdriver.common.appiumby import AppiumBy
+
+COMPANY_NAME_LABEL = (AppiumBy.ACCESSIBILITY_ID, "company-name")
+ANIMATION_DEMO_BUTTON = (AppiumBy.ACCESSIBILITY_ID, "go-to-animation-screen")
+SLIDE_MESSAGE_BUTTON = (AppiumBy.ID, "com.expandtesting.practice:id/btnSlideMessage")
+SLIDE_MESSAGE = (AppiumBy.ID, "com.expandtesting.practice:id/tvMessage")
